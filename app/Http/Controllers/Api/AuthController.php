3ba319ddd4b3ab7808946df5218372097bd428de
ngotu <ngotuananh9922@gmail.com>
2026-02-10 17:20:25 +0700

    [BE-0.1] update: refactor structure using extended MVC for login & register function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -2,54 +2,55 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Actions\Auth\LoginAction;
+use App\Exceptions\Auth\AccountLockedException;
+use App\Exceptions\Auth\LoginFailedException;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Services\AuthService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
 
 class AuthController extends Controller
 {
+   protected AuthService $authService;
+
+    /**
+     * @param AuthService $authService
+     */
+   public function __construct(AuthService $authService) {
+       $this->authService = $authService;
+   }
     public function login(LoginRequest $request): JsonResponse
     {
-        $credentials = $request->validated();
+        $data = $this->authService->login($request->validated());
 
-        if (! $token = auth('api')->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
-
-        if (!auth('api')->user()->is_active) {
-            auth('api')->logout();
-            return response()->json(['error' => 'This account was locked']);
-        }
-        return $this->respondWithToken($token);
+        return $this->respondSuccess($data, 'Login successful!');
     }
 
+    /**
+     * @param RegisterRequest $request
+     * @return JsonResponse
+     */
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $user = $this->authService->register($request->validated());
+        return $this->respondCreated(['user' => $user], 'Registration successful!');
+    }
+
+    /**
+     * @return JsonResponse
+     */
     public function me(): JsonResponse
     {
-        $user = auth('api')->user() -> load(['roles', 'profile']);
-
-        return response()->json($user);
+        $user = $this->authService->getProfile();
+        return $this->respondSuccess($user, 'Profile successful!');
     }
 
     public function logout(): JsonResponse
     {
-        auth('api')->logout();
-        return response()->json(['message' => 'Successfully logged out']);
-    }
-
-    public function refresh(): JsonResponse
-    {
-        return $this->respondWithToken(auth('api')->refresh());
-    }
-
-    protected function respondWithToken(string $token): JsonResponse
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth('api')->factory()->getTTL() * 60,
-            'user' => auth('api')->user()->load(['roles', 'profile']),
-        ]);
+        $this->authService->logout();
+        return $this->respondSuccess(null, 'Logout successful!');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,11 @@
 
 use App\Http\Controllers\Api\AuthController;
 
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
+Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
+    Route::post('register', [AuthController::class, 'register']);
+
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+    });
 });
